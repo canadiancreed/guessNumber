cc01677fc8cee38d3d2c6a845962c5ab664cfac6
Chris Reed <canadiancreed@users.noreply.github.com>
2018-02-05 21:50:42 -0500

    Create guess.go

---
+++ guess.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	low := 1
+	high := 100
+
+	fmt.Println("Please think of a number between", low, "and", high, ":")
+	fmt.Println("Press ENTER when ready")
+
+	scanner.Scan()
+
+	for {
+		guess := (low + high) / 2
+
+		fmt.Println("I guess the number is", guess)
+		fmt.Println("Is that:")
+		fmt.Println("(a) too high?")
+		fmt.Println("(b) too low?")
+		fmt.Println("(c) correct?")
+
+		scanner.Scan()
+
+		response := scanner.Text()
+
+		if response == "a" {
+			high = guess - 1
+		} else if response == "b" {
+			low = guess + 1
+		} else if response == "c" {
+			fmt.Println("You win!")
+			break
+		} else {
+			fmt.Println("What?!")
+		}
+	}
+}
